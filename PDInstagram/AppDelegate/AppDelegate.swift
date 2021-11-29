71e5afed32eb5ddc61256acea1f620a36302c5aa
PeterXiao <peide0427@gmail.com>
2021-11-29 17:19:54 -0500

    test

--- PDInstagram/AppDelegate/AppDelegate.swift
+++ PDInstagram/AppDelegate/AppDelegate.swift
@@ -12,7 +12,7 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
+    // test
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
